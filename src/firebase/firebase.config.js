5fa63351cda6c9c5b31af07042971637425b272d
Aditto <avikd906@proton.me>
2025-01-08 00:39:25 +0530

    firebase working properly on localhost. Faceing issue on Production . Trying to fix that

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -9,10 +9,10 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appIdd,
+  appId: import.meta.env.VITE_appId,
 };
 
-console.log(firebaseConfig);
+// console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
