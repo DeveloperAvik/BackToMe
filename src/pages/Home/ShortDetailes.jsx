2c68d3f25fb0756934281be73eef3a4dd9742501
Aditto <avikd906@proton.me>
2025-01-08 23:01:03 +0530

    Initial work complete for Shortdetailes.jsx

--- src/pages/Home/ShortDetailes.jsx
+++ src/pages/Home/ShortDetailes.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react"
 
 function ShortDetailes() {
+
+    const [allItems, setAllItems] = useState([])
+    const [lostItems, setLostItems] = useState([])
+    const [recoverItems, setRecoverItems] = useState([])
+
+
+    useState(() => {
+        fetch("https://backtome.vercel.app/items")
+            .then(res => res.json())
+            .then(data => setAllItems(data))
+    }, [])
+
+
+    useState(() => {
+        fetch("https://backtome.vercel.app/lostItems")
+            .then(res => res.json())
+            .then(data => setLostItems(data))
+    }, [])
+
+
+    useState(() => {
+        fetch("https://backtome.vercel.app/recoverItems")
+            .then(res => res.json())
+            .then(data => setRecoverItems(data))
+    }, [])
+
     return (
         <>
 
@@ -10,8 +37,8 @@ function ShortDetailes() {
 
                 <div className="card bg-base-100 w-96 shadow-xl">
                     <div className="card-body">
-                        <h2 className="card-title">Card title!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <h2 className="text-center font-bold text-2xl">{allItems.length}</h2>
+                        <p>The Number of Items Listed on our Website</p>
 
                     </div>
                 </div>
@@ -19,8 +46,8 @@ function ShortDetailes() {
 
                 <div className="card bg-base-100 w-96 shadow-xl">
                     <div className="card-body">
-                        <h2 className="card-title">Card title!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <h2 className="text-center font-bold text-2xl">{lostItems.length}</h2>
+                        <p>The Number of Lost Items Listed on our Website</p>
 
                     </div>
                 </div>
@@ -28,8 +55,8 @@ function ShortDetailes() {
 
                 <div className="card bg-base-100 w-96 shadow-xl">
                     <div className="card-body">
-                        <h2 className="card-title">Card title!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="text-center font-bold text-2xl">{recoverItems.length}</h2>
+                    <p>The Number of Recover Items Listed on our Website</p>
 
                     </div>
                 </div>
